15abbece24c62bcb7f5b358bf0d1313397734648
vinhcotam <vinhcotam04052001@gmail.com>
2022-05-31 16:51:19 +0700

    chỉnh lại ảnh

--- app/src/main/java/com/example/ideafood/Man1.java
+++ app/src/main/java/com/example/ideafood/Man1.java
@@ -21,7 +21,6 @@ public class Man1 extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
