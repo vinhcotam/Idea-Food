806be04898971b43c55f9497476b807c936ed8cc
h0angthe <79754508+h0angthe@users.noreply.github.com>
2022-06-01 22:40:27 +0700

    .

--- app/src/main/java/com/example/ideafood/Infodetail.java
+++ app/src/main/java/com/example/ideafood/Infodetail.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -11,12 +13,14 @@ import com.google.firebase.database.DatabaseReference;
 public class Infodetail extends AppCompatActivity {
     TextView tvname,tvemail;
     DatabaseReference database;
+    Button btn_doipass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infodetail);
         tvname = findViewById(R.id.tv_name);
         tvemail = findViewById(R.id.tv_email);
+        btn_doipass = findViewById(R.id.btn_doimk);
 
 
         Intent intent =getIntent();
@@ -27,8 +31,13 @@ public class Infodetail extends AppCompatActivity {
         String Level = bundle.getString("Level");
         tvname.setText(Username);
         tvemail.setText(Email);
+        btn_doipass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Infodetail.this,changepass.class);
+                startActivity(intent1);
+            }
+        });
     }
-    void loadpost(){
 
-    }
 }
--- app/src/main/java/com/example/ideafood/changepass.java
+++ app/src/main/java/com/example/ideafood/changepass.java
@@ -3,14 +3,10 @@ package com.example.ideafood;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,27 +17,23 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class User extends AppCompatActivity {
-    ListView listView;
-    ArrayList<Nguoidung> listnguoidung= null;
-    AdapterUser adapterUser;
+public class changepass extends AppCompatActivity {
+    Button btn_pass;
+    EditText edt_pass,edt_pass2;
     DatabaseReference database;
-    EditText edText_search;
-    ImageButton imgBtn;
+    ArrayList<Nguoidung> listnguoidung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
-        listView = findViewById(R.id.listview_user);
-        ConnectDB();
-        loaduser();
-        infodetail();
-    }
-    void ConnectDB(){
+        setContentView(R.layout.activity_changepass);
+        btn_pass = findViewById(R.id.btn_doi);
+        edt_pass = findViewById(R.id.edt_pass);
+        edt_pass2 = findViewById(R.id.edt_pass2);
         database = FirebaseDatabase.getInstance().getReference();
-    }
-    void loaduser(){
+        String pass1 = edt_pass.getText().toString();
+        String pass2 = edt_pass2.getText().toString();
+
         Query allAccount = database.child("Account");
         allAccount.addValueEventListener(new ValueEventListener() {
             @Override
@@ -51,26 +43,17 @@ public class User extends AppCompatActivity {
                     Nguoidung nguoidung = item.getValue(Nguoidung.class);
                     listnguoidung.add(nguoidung);
                 }
-                adapterUser = new AdapterUser(User.this, R.layout.lv_user, listnguoidung);
-                listView.setAdapter(adapterUser);
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
-    }
-    void infodetail(){
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        btn_pass.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(User.this,Infodetail.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("Username",listnguoidung.get(i).username);
-                bundle.putString("Password",listnguoidung.get(i).password);
-                bundle.putString("Email",listnguoidung.get(i).email);
-                bundle.putString("Level",listnguoidung.get(i).level);
-                intent.putExtras(bundle);
-                startActivity(intent);
+            public void onClick(View view) {
+                
+
             }
         });
     }
