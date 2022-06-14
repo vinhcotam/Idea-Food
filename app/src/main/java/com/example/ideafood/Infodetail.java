97f1784347c7d5621227f562139bf640fb9fafed
Dương <leduong.w01@gmail.com>
2022-06-14 20:27:05 +0700

    sửa lỗi khi ấn xem bài viết ở thông tin tài khoản

--- app/src/main/java/com/example/ideafood/Infodetail.java
+++ app/src/main/java/com/example/ideafood/Infodetail.java
@@ -125,16 +125,10 @@ public class Infodetail extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            Intent intent1 = new Intent(Infodetail.this,Postdetail.class);
+            Intent intent1 = new Intent(Infodetail.this,DetailPost.class);
             Bundle bundle1 = new Bundle();
             bundle1.putString("postid",listposts.get(i).postid);
-            bundle1.putString("postname",listposts.get(i).postname);
-            bundle1.putString("header",listposts.get(i).header);
-            bundle1.putString("date",listposts.get(i).date);
-            bundle1.putString("category",listposts.get(i).category);
-            bundle1.putBoolean("status",listposts.get(i).isStatus());
-            bundle1.putString("content1",listposts.get(i).content_post.get(0));
-            bundle1.putString("content2",listposts.get(i).content_post.get(1));
+            bundle1.putString("username", username);
             intent1.putExtras(bundle1);
             startActivity(intent1);
             }
